11d5425fe1f87f73964997e12a647c26d0c3f376
Arthur Zim <arthur@duostecnologia.com.br>
2024-06-07 14:41:51 -0300

    fix hello world

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World 2!'),
+          child: Text('Hello World 2024'),
         ),
       ),
     );
